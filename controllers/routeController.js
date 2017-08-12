f9c5d20a0cfc9737a20c0275ca0cab883d66bb96
Rachel Thiim <rachel.thiim@gmail.com>
2017-08-12 13:11:02 -0500

    Added routes for all pages

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,9 +1,28 @@
-var express = require("express");
+const express = require("express");
+const router = express.Router();
 
-var router = express.Router();
+router.get('/', function (req, res) {
+  res.render('index');
+});
 
-router.get("/", function (req, res) {
-  res.render("index");
+router.get('/contact', function (req, res) {
+  res.render('contact');
+});
+
+router.get('/custom', function (req, res) {
+  res.render('custom');
+});
+
+router.get('/ideas', function (req, res) {
+  res.render('ideas');
+});
+
+router.get('/pricing', function (req, res) {
+  res.render('pricing');
+});
+
+router.get('/ordering', function (req, res) {
+  res.render('ordering');
 });
 
 module.exports = router;
