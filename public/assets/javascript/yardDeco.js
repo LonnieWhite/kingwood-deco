99720954def88ec72516811f2547dcde0fd89d5a
Rachel Thiim <rachel.thiim@gmail.com>
2017-08-24 19:12:48 -0500

    Significant style changes and form 'completion'

--- public/assets/javascript/yardDeco.js
+++ public/assets/javascript/yardDeco.js
@@ -6,4 +6,25 @@ $(document).ready(function () {
     ride: 'carousel',
     wrap: true
   });
+
+  $('input.form-control').on('change', function () {
+    var orderEmail = {
+      name: $('#formName').val().trim(),
+      email: $('#formEmail').val().trim(),
+      color: $('#formColor').value,
+      phone: $('#formPhone').val(),
+      address: $('#formStreet').val().trim() + '' + $('#formStreet2').val().trim() + '' + $('#formCity').val().trim() + '' + $('#formZip').val().trim(),
+      instructions: $('#formInstructions').val().trim(),
+      date: $('#formDate').val(),
+      time: $('#formTime').val()
+    }
+
+
+    $('#formSubmit').on('submit', function (event) {
+      event.preventDefault;
+      if ($('#formPolicy')) {
+        console.log(orderEmail);
+      }
+    });
+  });
 });
